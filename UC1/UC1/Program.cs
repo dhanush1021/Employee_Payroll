f29f85acb3dafd3145c9e4e55ed5b903103ce775
Dhanush <dhanushdybala10@gmail.com>
2024-01-18 19:11:05 +0530

    [Dhanush] Add: Use Case 2 program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -4,22 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace UC2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int val = random.Next(2);
-            if (val == 0)
-            {
-                Console.WriteLine("Absent");
-            }
-            else
-            {
-                Console.WriteLine("Present");
-            }
+            int val = 8 * 20;
+            Console.WriteLine($"Wages : {val}");
             Console.ReadKey();
         }
     }
