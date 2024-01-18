b0486a3d127badd212e35e47333cab390662eed0
Dhanush <dhanushdybala10@gmail.com>
2024-01-18 19:13:06 +0530

    [Dhanush] Add: Use Case 3 program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC2
+namespace UC3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int val = 8 * 20;
-            Console.WriteLine($"Wages : {val}");
+            Random rnd = new Random();
+            int val = rnd.Next(3);
+            if (val == 0)
+            {
+                Console.WriteLine("Absent");
+            }
+            else if (val == 1)
+            {
+                Console.WriteLine("Part time");
+            }
+            else
+            {
+                Console.Write("Full time");
+            }
             Console.ReadKey();
         }
     }
