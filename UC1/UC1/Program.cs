10c74efb993c5f28d8d0d9d593a4278c53aabd99
Dhanush <dhanushdybala10@gmail.com>
2024-01-18 19:17:24 +0530

    [Dhanush] Add: Use Case 5 program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -4,26 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC4
+namespace UC5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int val = rnd.Next(3);
-            switch (val)
-            {
-                case 0:
-                    Console.WriteLine("Absent");
-                    break;
-                case 1:
-                    Console.WriteLine("Part Time");
-                    break;
-                case 2:
-                    Console.WriteLine("Full Time");
-                    break;
-            }
+            int val = 8 * 20 * 20;
+            Console.WriteLine($"Monthly Wages : {val}");
             Console.ReadKey();
         }
     }
