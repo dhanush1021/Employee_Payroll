d5c8e905cce9f272f368510738716d0f4b3b46d9
Dhanush <dhanushdybala10@gmail.com>
2024-01-18 19:07:39 +0530

    [Dhanush] Add: Use Case 1 program

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int val = random.Next(2);
+            if (val == 0)
+            {
+                Console.WriteLine("Absent");
+            }
+            else
+            {
+                Console.WriteLine("Present");
+            }
+            Console.ReadKey();
+        }
+    }
+}
